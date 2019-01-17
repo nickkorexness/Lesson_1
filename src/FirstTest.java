ee369fbeb97e170f78547517deb70f233978fb1d
Nickolay <>
2019-01-17 22:19:40 +0200

    no message

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -146,7 +146,187 @@ public class FirstTest {
     }
 
 
-   
+    @Test
+    public void ex5_two_articles()
+    {
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                10
+        );
+
+        //Добавляем первую статью и сохраняем в новую папку
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find search input",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cant find Java Title",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.widget.ImageView' and @index='2']"),
+                "Cant find 'more option' button",
+                20
+        );
+
+
+        waitForElementAndClick(
+                //By.xpath("//*[@text='Add to reading list']"),
+                By.xpath("//*[@class='android.widget.LinearLayout' and @index='2']"),
+                "cant fing add to reading list button",
+                20
+        );
+
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cant find 'got it' overlay button",
+                15
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "cant find input to set the name of the folder",
+                15
+        );
+
+        String name_of_folder = "Learning";
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "cant find input to set the name of the folder",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text ='OK']"),
+                "Cant click on OK button to save the folder name",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cant close the article",
+                15
+        );
+
+        //Добавляем вторую статью и сохраняем в новую папку
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "JavaScript",
+                "Cannot find search input",
+                10
+        );
+
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='JavaScript']"),
+                "Cannot find search input",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cant find JavaScript Title",
+                15
+        );
+
+        String title_actual = waitForElementAndGetAttribute(
+                        By.xpath("//*[@class='android.widget.TextView' and @index='0']"),
+                        "text",
+                        "cant find text or article",
+                        10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.widget.ImageView' and @index='2']"),
+                "Cant find 'more option' button",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@class='android.widget.LinearLayout' and @index='2']"),
+                "Cant find 'add ro reading list' button",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text ='" + name_of_folder + "']"),
+                "Cant click on OK button to save the folder name",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cant close the article",
+                15
+        );
+
+        // открываем папку и удаляем
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cant open 'my lists' screen ",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text ='" + name_of_folder + "']"),
+                "Cant find  folder",
+                15
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text ='Java (programming language)']"),
+                "Cant find saved article"
+        );
+
+        waitForElementNotPresent(By.xpath("//*[@text ='Java (programming language)']"),
+                "Cant delete saved article",
+                10
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@text ='JavaScript']"),
+                "Cant find article with Javascript entry",
+                10
+        );
+
+        String title_expected = waitForElementAndGetAttribute(
+                By.xpath("//*[@class='android.widget.TextView' and @index='0']"),
+                "text",
+                "cant find text or article",
+                10
+        );
+
+        Assert.assertEquals(
+                "article title is not equals",
+                title_actual,
+                title_expected
+        );
+    }
 
     @Test
     public void ex6_title_assert()
