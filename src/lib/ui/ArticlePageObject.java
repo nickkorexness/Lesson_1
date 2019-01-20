7592f476792df186d36ccf2004af83815eea0c02
Nickolay <>
2019-01-20 10:49:20 +0200

    no message

--- src/lib/ui/ArticlePageObject.java
+++ src/lib/ui/ArticlePageObject.java
@@ -114,8 +114,5 @@ public class ArticlePageObject extends MainPageObject {
         );
     }
 
-    public void checkArticleTitleWithTimeout(int timeout)
-    {
-        this.waitForElementPresent(By.id(ARTICLE_CLOSE_BTN),"cant find article element on article page with "+ timeout + " timeout", timeout);
-    }
+
 }
